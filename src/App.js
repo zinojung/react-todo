706a34b5dd71b64240358e5b006f1ef9909ed028
ZINO\46fir <46firstmate@gmail.com>
2018-02-01 14:38:32 +0900

    Set static page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ class App extends Component {
     return (
       <div>
         <h1>Hello World</h1>
+        <form>
+          <input type="text" />
+          <button>Add Todo</button>
+        </form>
+        <div>
+          <div>
+            <label>
+              <input type="checkbox" />
+              <span>This is test todo</span>
+            </label>
+            <button>Remove</button>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" />
+              <span>This is test todo</span>
+            </label>
+            <button>Remove</button>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" />
+              <span>This is test todo</span>
+            </label>
+            <button>Remove</button>
+          </div>
+        </div>
       </div>
     );
   }
